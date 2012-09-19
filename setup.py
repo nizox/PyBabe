2be2d27b630be83dd1de80770d19d2d0dd5fcb96
Nicolas Vivet <nizzox@gmail.com>
2012-09-19 19:42:36 +0200

    Add a setup.py file and remove uncomplete files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="PyBabe",
+    version="0.0",
+    packages=["pybabe",],
+    license="Unknown",
+    description="PyBabe is a micro ETL framework.",
+    author="Florian Douetteau",
+    url='https://github.com/fdouetteau/PyBabe.git',
+)
