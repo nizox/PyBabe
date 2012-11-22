00ff55751b5f00d96cdcff8bd148bfda54c92f46
Nicolas Vivet <nizzox@gmail.com>
2012-11-22 11:52:16 +0100

    Add format_json default handler

--- pybabe/format_json.py
+++ pybabe/format_json.py
@@ -1,7 +1,13 @@
-
+import datetime
 import json
 import codecs
 from base import StreamHeader, BabeBase, StreamFooter
+
+
+def default_json_handler(obj):
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    raise TypeError
 
 
 def pull(format, stream, kwargs):
@@ -21,13 +27,15 @@ def pull(format, stream, kwargs):
 
 def push(format, metainfo, instream, outfile, encoding, **kwargs):
     outstream = codecs.getwriter(kwargs.get('encoding', 'utf8'))(outfile)
+    default_handler = kwargs.get('default_json_handler', default_json_handler)
     for row in instream:
         if isinstance(row, StreamFooter):
             break
         elif isinstance(row, StreamHeader):
             metainfo = row
         else:
-            outstream.write(json.dumps(dict(zip(metainfo.fields, row))))
+            outstream.write(json.dumps(dict(zip(metainfo.fields, row)),
+                default=default_handler))
             outstream.write("\n")
     outstream.flush()
 
